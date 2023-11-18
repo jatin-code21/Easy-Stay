8fbe7d2ac92767a7ede51e0b210b73536bd7624c
TheBeast <ozajatin9309@gmail.com>
2023-11-18 18:13:18 +0530

    origin url changed

--- api/index.js
+++ api/index.js
@@ -30,7 +30,7 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: ["https://easy-stay-frontend.vercel.app", "http://localhost:5173"],
   })
 );
 
