b9a53f072347c1469d20e05acb53accb1f52f732
TheBeast <ozajatin9309@gmail.com>
2023-11-18 18:06:07 +0530

    frontend baseurl changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,7 @@ import BookingsPage from "./pages/BookingsPage";
 import BookingPage from "./pages/BookingPage";
 
 // axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
-axios.defaults.baseURL = "http://localhost:4000";
+axios.defaults.baseURL = "https://easy-stay-api.vercel.app";
 axios.defaults.withCredentials = true;
 
 function App() {
