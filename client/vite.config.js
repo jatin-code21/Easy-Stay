5e7b6a3460795ff142dca513d1cb01771b2e0bff
TheBeast <ozajatin9309@gmail.com>
2023-12-08 14:11:22 +0530

    chunk size changed

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  external: ['source-map-js'],
-})
+  external: ["source-map-js"],
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
+});
 
 // module.exports = {
 //   define: {
